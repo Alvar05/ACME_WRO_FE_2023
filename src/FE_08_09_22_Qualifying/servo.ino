0d6e6085c823c358421f1955cb874b633292c11f
Alvar05 <92118932+Alvar05@users.noreply.github.com>
2022-09-08 17:54:17 +0200

    Delete src/FE_08_09_22_Qualifying directory

--- src/FE_08_09_22_Qualifying/servo.ino
+++
@@ -1,9 +0,0 @@
-void setupServo() { //Initiates servo
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
-  direccion.setPeriodHertz(50);
-  direccion.attach(17);
-  direccion.write(90);
-}
